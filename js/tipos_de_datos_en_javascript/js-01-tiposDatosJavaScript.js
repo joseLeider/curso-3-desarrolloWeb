dcb2bae648c0f85f386fc571afa7aec50fe6f4eb
joseLeider <180058244+joseLeider@users.noreply.github.com>
2024-11-25 14:34:10 -0500

    Agregar Mas tipos de datos en javascript

--- js/tipos_de_datos_en_javascript/js-01-tiposDatosJavaScript.js
+++ js/tipos_de_datos_en_javascript/js-01-tiposDatosJavaScript.js
@@ -19,3 +19,42 @@ var objeto = {
     telefono : 45313668
 }
 console.log(typeof objeto);
+
+// Tipo de dato boolean (true, false)
+var bandera = false;
+console.log(bandera);
+console.log(typeof bandera);
+
+// Tipo de dato function
+function miFuncion() {}
+console.log(miFuncion);
+console.log(typeof miFuncion);
+
+// Tipo de dato Symbol
+var simbolo = Symbol("mi simbolo");
+console.log(simbolo);
+console.log(typeof simbolo);
+
+// Tipo clase es una function
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+console.log(Persona);
+console.log(typeof Persona);
+
+// Tipo undefined
+var x;
+console.log(x);
+console.log(typeof x);
+
+x = undefined;
+console.log(x);
+console.log(typeof x);
+
+// Tipo de dato object: null = ausencia de valor
+var y = null;
+console.log(y);
+console.log(typeof y);
