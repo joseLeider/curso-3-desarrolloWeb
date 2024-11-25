9953dd8231089ad1aae07671ff890d52a6569031
joseLeider <180058244+joseLeider@users.noreply.github.com>
2024-11-25 18:12:22 -0500

    Agregar Operadores aritmeticos

---
+++ js/operadores_en_javascript/js-01-operadoresAritmeticos.js
@@ -0,0 +1,19 @@
+let a = 3, b = 2;
+
+let z = a + b;
+console.log("Resultado de la suma: " + z);
+
+z = a - b;
+console.log("Resultado de la resta: " + z);
+
+z = a * b;
+console.log("Resultado de la multiplicacion: " + z);
+
+z = a / b;
+console.log("Resultado de la división: " + z);
+
+z = a % b;
+console.log("Resultado del operador modulo (residuo): " + z);
+
+z = a ** b;
+console.log("Resultado del operador exponente: " + z);
