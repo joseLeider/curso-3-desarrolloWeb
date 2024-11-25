edea67f874eafc00721258aae6f0100cab80b218
joseLeider <180058244+joseLeider@users.noreply.github.com>
2024-11-24 23:51:50 -0500

    Agregar Tipos de datos en javascript

---
+++ js/tipos_de_datos_en_javascript/js-01-tiposDatosJavaScript.js
@@ -0,0 +1,15 @@
+// Tipo de datos string
+var nombre = "Felipe";
+console.log(nombre);
+
+// Tipo de dato numerico
+var numero = 1000;
+console.log(numero);
+
+// Tipo de dato object
+var objeto = {
+    nombre : "Adriano",
+    apellido : "ruiz",
+    telefono : 45313668
+}
+console.log(objeto);
