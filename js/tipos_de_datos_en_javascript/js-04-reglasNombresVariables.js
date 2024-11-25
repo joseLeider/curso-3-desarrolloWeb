445c161ab15e48cc3bc859ba6c4127f4cb6c7c66
joseLeider <180058244+joseLeider@users.noreply.github.com>
2024-11-25 17:46:20 -0500

    Agregar Reglas para definir nombres de variables

---
+++ js/tipos_de_datos_en_javascript/js-04-reglasNombresVariables.js
@@ -0,0 +1,15 @@
+//! JavaScript es sensible a los nombres de las variables, funciones y otros identificadores (mayusculas y minusculas)
+
+let nombreCompleto = "Pedro Muñoz";
+let nombrecompleto = "Francisco Torre";
+console.log(nombreCompleto); // 'Pedro Muñoz'
+console.log(nombrecompleto); // 'Francisco Torre'
+// console.log(NombreCompleto); // NombreCompleto no esta definida
+
+
+// Reglas para definir nombres de variables
+let a1nombreCompleto;
+let _nombreCompleto;
+let $nombreCompleto;
+
+//let 1nombreCompleto; // No está permitido iniciar el nombre de una variable con un número.
