5d29de91aebad1d499ffdb7cc00b4d91ce14ed4a
joseLeider <180058244+joseLeider@users.noreply.github.com>
2024-11-24 23:31:41 -0500

    Agregar Hola mundo con javascript

---
+++ js/introduccion_a_javascript/holaMundo.js
@@ -0,0 +1,15 @@
+var nombre = "Pablito";
+
+console.log(nombre);
+
+//! Se instalo node.js y las extensiones Quokka.js, Better Comments
+
+// Para ejecutar un programa es necesario desplegarlo sobre un navegador web para que el navegador pueda intepretar el codigo.
+// Se usa Quokka con el fin de ejecutar los programas js sin el script de un archivo html, un servidor o un archivo node.js.
+
+/* Ctrl + Shift + P: Escribimos Quokka.js
+Para seleccionar las opciones de crear, iniciar o finalizar la ejecucion de un archivo js con Quokka, entre otros.*/
+
+// Para Crear un nuevo archivo js con la extension de Quokka seleccionamos: New JavaScript File
+// Para Iniciar la ejecución de Quokka en un archivo existente seleccionamos: Togglo (Start/Stop) on Current File o Start on Current File
+// Para Finalizar la ejecución seleccionamos de nuevo la opción: Togglo (Start/Stop) on Current File o Start on Current File
