affbf67016a03c97ad577fe4bd8ad53a151cb43e
joseLeider <180058244+joseLeider@users.noreply.github.com>
2024-11-26 14:49:35 -0500

    Agregar Operadores de asignacion

---
+++ js/operadores_en_javascript/js-04-operadoresAsignacion.js
@@ -0,0 +1,25 @@
+// Operador de asignación
+
+let a = 1;
+console.log(a);
+
+
+// Operadores de asignación compuestos: Son operadores que combinan una operación artitmética o lógica con una asignación, simplificando el codigo.
+
+// Suma y asigna
+a += 3; // a = a + 3
+console.log(a);
+
+// Resta y asigna
+a -= 2; // a = a - 2
+console.log(a);
+
+// Potencia y asigna
+a **= 4;
+console.log(a); // a = a ** 4
+
+/*
+    *= Multiplica y asigna
+    /= Divide y asigna
+    %= Calcula el módulo y asigna
+*/
