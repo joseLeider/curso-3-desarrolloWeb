f2a107e03df40fa497b9e95d367b7edb578683b4
joseLeider <180058244+joseLeider@users.noreply.github.com>
2024-11-26 15:07:00 -0500

    Agregar Operador distinto

---
+++ js/operadores_en_javascript/js-06-operadoresDistintoA.js
@@ -0,0 +1,7 @@
+let a = 3, b = 2, c = "3";
+
+let z = a != c; // Compara el valor sin importar el tipo de dato
+console.log(z); // true
+
+z = a !== c; // Compara los valores y su tipo de dato
+console.log(z); // false
