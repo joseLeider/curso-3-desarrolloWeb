13fd5ae9eabbe5018f80c837d590cc20c097294b
joseLeider <180058244+joseLeider@users.noreply.github.com>
2024-11-27 14:16:02 -0500

    Agregar Operador or

---
+++ js/operadores_en_javascript/js-11-operadorLogicoOr.js
@@ -0,0 +1,7 @@
+let vacaciones = false, diaDescanso = true;
+
+if(vacaciones || diaDescanso){//Operador Or, al menos una condición debe ser verdadera
+    console.log("Padre puede asistir al juego del hijo");
+}else{
+    console.log("El padre no puede asistir al juego del hijo");
+}
