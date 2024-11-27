c0378f97a1bb21941e35029ed31bb5ee0dca73c9
joseLeider <180058244+joseLeider@users.noreply.github.com>
2024-11-27 13:58:55 -0500

    Agregar Operador and

---
+++ js/operadores_en_javascript/js-10-operadorLogicoAnd.js
@@ -0,0 +1,8 @@
+let a = 4;
+let valorMinimo = 0, valorMaximo = 10;
+
+if(a >= valorMinimo && a <= valorMaximo){//Operador And, ambas condiciones deben ser verdaderas
+    console.log("Dentro de rango");
+}else{
+    console.log("Fuera de rango");
+}
