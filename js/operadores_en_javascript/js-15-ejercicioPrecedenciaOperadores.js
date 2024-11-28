44b02339ebe57c464901308395fe9599cedc184d
joseLeider <180058244+joseLeider@users.noreply.github.com>
2024-11-28 11:54:17 -0500

    Agregar Ejemplos precedencia de operadores

---
+++ js/operadores_en_javascript/js-15-ejercicioPrecedenciaOperadores.js
@@ -0,0 +1,13 @@
+let x = 5;
+let y = 10;
+let z = ++x + y--; // x = 6 , y = 9, z = 16
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+let resultado = 4 + 5 * 6 / 3;
+console.log(resultado); // 14
+
+resultado = (4 + 5) * 6 / 3;
+console.log(resultado); // 18
