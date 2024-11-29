ba82432433c2d77593a34574b70c89a7a745f9be
joseLeider <180058244+joseLeider@users.noreply.github.com>
2024-11-29 12:06:39 -0500

    Agregar ciclo for

---
+++ js/ciclos_en_javascript/cicloFor.js
@@ -0,0 +1,7 @@
+/*Es una estructura de control que repite un bloque de código un número específico de veces. Es ideal cuando conoces de antemano cuántas iteraciones necesitas.*/
+
+for (let contador = 0; contador < 3; contador++) {
+    console.log(contador);
+}
+
+console.log("Fin ciclo for");
