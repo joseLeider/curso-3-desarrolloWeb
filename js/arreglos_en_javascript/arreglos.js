c8ffafbc9bbfff1394d2e71d08ba2340e161d3bb
joseLeider <180058244+joseLeider@users.noreply.github.com>
2024-11-30 18:46:10 -0500

    Agregar Arreglos en javascript

---
+++ js/arreglos_en_javascript/arreglos.js
@@ -0,0 +1,6 @@
+// Un arreglo es una estructura de datos que almacena una colección de valores, organizados en un orden especifico
+// Puede contener diferente tipos de datos: números, cadenas, objetos, etc.
+// Es dinámico, se puede agregar, eliminar o modificar elementos.
+
+const colores = ['amarillo', 'azul', 'rojo', 'verde'];
+console.log(colores);
