19c6ecd5103813d941fc7c676389e4a324ad08ab
joseLeider <180058244+joseLeider@users.noreply.github.com>
2024-11-30 23:54:32 -0500

    Agregar Palabra return en funciones

---
+++ js/funciones_en_javascript/palabraReturn.js
@@ -0,0 +1,9 @@
+
+function miFuncion(a, b) {
+    // La palabra clave "return" devuelve la suma de los parámetros a y b y finaliza la ejecución de la función
+    // Si no se usa return, la función devuelve undefined por defecto, asi sea almacenada en una variable.
+    return a + b;
+}
+
+let resultado = miFuncion(4,5);
+console.log(resultado);
