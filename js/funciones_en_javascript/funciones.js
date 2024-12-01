f1f0ccd3defb84e1eff803ac704f6bdf97bd0b0d
joseLeider <180058244+joseLeider@users.noreply.github.com>
2024-11-30 23:37:54 -0500

    Agregar Funciones en javascript

---
+++ js/funciones_en_javascript/funciones.js
@@ -0,0 +1,13 @@
+// Una función en JavaScript es un bloque de código reutilizable que realiza una tarea específica. Puede recibir parámetros (datos de entrada) y devolver un resultado.
+
+
+// Llamada de la función antes de la declaración
+miFuncion(6, 9);
+
+// Declaración de la función
+function miFuncion(a, b) {
+    console.log("Suma: " + (a + b));
+}
+
+// Llamada de la función despues de la declaración
+miFuncion(2,2);
