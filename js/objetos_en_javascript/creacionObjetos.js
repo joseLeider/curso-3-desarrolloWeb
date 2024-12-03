4ef104b29370ed26a374451a2c695e866dd0aa16
joseLeider <180058244+joseLeider@users.noreply.github.com>
2024-12-02 23:46:42 -0500

    Agregar Creacion de objetos

---
+++ js/objetos_en_javascript/creacionObjetos.js
@@ -0,0 +1,10 @@
+// Otra forma de crear objetos: usando el constructor 'Object'
+
+let persona = new Object(); // new Object(): Crea un objeto vacío.
+
+// Agregamos propiedades al objeto 'persona' usando la notación de punto (obj.propiedad = valor).
+persona.nombre = 'Sebastian';
+persona.direccion = 'San Francisco';
+persona.telefono = '15426298454'; // Propiedad 'telefono' con el valor '15426298454'
+
+console.log(persona.telefono);
