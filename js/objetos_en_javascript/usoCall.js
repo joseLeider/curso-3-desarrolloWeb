90f2b494c1aa8e5705de299261f3e646192ff133
joseLeider <180058244+joseLeider@users.noreply.github.com>
2024-12-04 23:46:16 -0500

    Agregar Uso del metodo call

---
+++ js/objetos_en_javascript/usoCall.js
@@ -0,0 +1,24 @@
+let persona1 = {
+    nombre: 'Juan',
+    apellido: 'Sanchez',
+    nombreCompleto:function(){
+        return this.nombre + ' ' + this.apellido; 
+    }
+}
+
+let persona2 = {
+    nombre: 'Juliana',
+    apellido: 'Triana'
+}
+
+// Metodo Call
+// Permite usar un método definido en un objeto (persona1) para trabajar con las propiedades de otro objeto (persona2).
+// Es útil cuando queremos reutilizar métodos de un objeto en otro objeto que no tiene definido ese método.
+
+// Invocamos el método 'nombreCompleto' desde persona1
+console.log(persona1.nombreCompleto()); 
+// Output: 'Juan Sanchez' (usa los valores de persona1)
+
+// Usamos 'call' para invocar el método de persona1 con los datos de persona2
+console.log(persona1.nombreCompleto.call(persona2)); 
+// Output: 'Juliana Triana' (usa los valores de persona2 como 'this')
