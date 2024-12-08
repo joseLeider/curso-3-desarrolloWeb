7c45bc7b0f43eda90126273aac4f558ed206a6ad
joseLeider <180058244+joseLeider@users.noreply.github.com>
2024-12-07 23:59:00 -0500

    Agregar Atributos estaticos

--- js/palabra_static_en_javascript/metodoStatic.js
+++ js/palabra_static_en_javascript/metodoStatic.js
@@ -1,7 +1,16 @@
 class Persona {
+
+    // Atributos estáticos son ideales para datos que son comunes a todas las instancias, como un contador de objetos creados.
+    // En este caso, Persona.contadorObjetosPersona cuenta cuántos objetos Persona o derivados (Empleado) han sido creados.
+
+    // Sirve para contar cuántas instancias se han creado de Persona o de sus clases derivadas (como Empleado).
+    static contadorObjetosPersona = 0; // Definimos un atributo estatico que solo pertenecera a la clase y no al objeto
+
     constructor(nombre, apellido) {
         this._nombre = nombre;
         this._apellido = apellido;
+        Persona.contadorObjetosPersona++; // Incrementa el contador cada vez que se crea una instancia
+        console.log('Se incrementa contador: '+ Persona.contadorObjetosPersona);
     }
 
     get nombre() {
@@ -62,9 +71,9 @@ class Empleado extends Persona{
     }
 }
 
-let persona1 = new Persona('Jose', 'Rodriguez');
+let persona1 = new Persona('Jose', 'Rodriguez'); // Se incrementa contador: 1
 console.log(persona1);
-let empleado1 = new Empleado('Paula', 'Puentes', 'Sistemas');
+let empleado1 = new Empleado('Paula', 'Puentes', 'Sistemas'); // Se incrementa contador: 2
 console.log(empleado1);
 console.log(empleado1.nombre);
 console.log(empleado1.nombreCompleto());
@@ -82,3 +91,7 @@ Persona.saludar2(persona1); // Pasa la instancia persona1 como argumento
 // Métodos estáticos también se heredan
 Empleado.saludar1(); // Llama al método static heredado de Persona
 Empleado.saludar2(empleado1); // Pasa la instancia empleado1 como argumento
+
+console.log(persona1.contadorObjetosPersona); // undefined (no se puede acceder desde una instancia)
+console.log(Persona.contadorObjetosPersona); // 2 (se accede desde la clase directamente)
+console.log(Persona.contadorObjetosPersona);
