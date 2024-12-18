713936c50aca57eae62b99229dff209328848fde
joseLeider <180058244+joseLeider@users.noreply.github.com>
2024-12-17 22:59:09 -0500

    Agregar Clase computadora con agregaciones

--- js/ejercicio_mundoPC/claseRaton.js
+++ js/ejercicio_mundoPC/claseRaton.js
@@ -80,6 +80,37 @@ class Monitor{
 }
 
 
+/* La agregación en clases se refiere a una relación en la que una clase (por ejemplo, Computadora) contiene referencias
+a otras clases (por ejemplo, Monitor, Raton, y Teclado) como atributos. */
+
+/* La clase Computadora no crea los objetos Monitor, Raton ni Teclado, sino que recibe instancias ya existentes en su constructor. Esto demuestra la relación de agregación. */
+
+// Clase principal Computadora con agregación
+class Computadora{
+
+    static contadorComputadoras = 0;
+    // En esta clase, se reciben objetos externos de tipo Monitor, Raton y Teclado en el constructor.
+    constructor(nombre, monitor, teclado, raton){
+        this._idComputadora = ++Computadora.contadorComputadoras;
+        this._nombre = nombre;
+        this._monitor = monitor; // Agregación
+        this._teclado = teclado; // Agregación
+        this._raton = raton; // Agregación
+    }
+
+    get idComputadora(){
+        return this._idComputadora;
+    }
+
+    // Cada clase auxiliar (Monitor, Raton y Teclado) tiene su propio método toString() para describir el objeto.
+
+    // El método toString() Concatena la información de las clases auxiliares llamando a sus respectivos métodos.
+    toString(){
+        return `Computadora ${this._idComputadora}: ${this._nombre} \n ${this._monitor} \n ${this._teclado} \n ${this._raton}`;
+    }
+}
+
+
 let raton1 = new Raton('USB', 'HP');
 console.log(raton1.toString());
 
@@ -98,3 +129,12 @@ let monitor1 = new Monitor('Asus', 14);
 let monitor2 = new Monitor('Dell', 27);
 console.log(monitor1.toString());
 console.log(monitor2.toString());
+
+
+// Crear objeto principal (Computadora) con agregación
+let computadora1 = new Computadora('Asus', monitor1, raton1, teclado1);
+console.log(computadora1.toString());
+// console.log(`${computadora1}`); mismo resultado con template string
+
+let computadora2 = new Computadora('Armada', monitor2, raton2, teclado2);
+console.log(`${computadora2}`);
