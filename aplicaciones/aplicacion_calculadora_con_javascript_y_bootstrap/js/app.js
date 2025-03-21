ef05d82f0ec3409f1ccdbab614d95cd8ad2d3034
joseLeider <180058244+joseLeider@users.noreply.github.com>
2025-03-21 15:36:13 -0500

    Agregar Despliegue del resultado aplicacion calculadora

--- aplicaciones/aplicacion_calculadora_con_javascript_y_bootstrap/js/app.js
+++ aplicaciones/aplicacion_calculadora_con_javascript_y_bootstrap/js/app.js
@@ -1,1 +1,24 @@
 console.log('aplicación Calculadora');
+
+function sumar(){
+    // Obtiene el formulario por su ID
+    const forma = document.getElementById('forma');
+
+    // Obtiene los elementos input del formulario por su atributo "name"
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+
+    // Extrae los valores de los elementos input, los convierte a enteros y los suma
+    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+
+    // Verifica si el resultado no es un número y asigna un mensaje de error
+    if(isNaN(resultado)){
+        resultado = 'La operación no incluye números';
+    }
+
+    // Actualiza el resultado en el elemento con ID 'resultado'
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+
+    // Muestra el resultado en la consola
+    console.log(`Resultado: ${resultado}`);
+}
