dbc1e7dd3640c7e0294e855e185c51194afb9209
joseLeider <180058244+joseLeider@users.noreply.github.com>
2025-03-22 21:43:58 -0500

    Agregar Listado de personas con javascript

--- aplicaciones/aplicacion_listado_de_personas_con_javascript/js/app.js
+++ aplicaciones/aplicacion_listado_de_personas_con_javascript/js/app.js
@@ -1,1 +1,14 @@
-console.log('Listado de personas');
+const personas = [
+    new Persona('Julían', 'Beltran'),
+    new Persona('Carolina', 'Díaz')
+];
+
+function mostrarPersonas(){
+    console.log('Mostrar personas...');
+    let texto = '';
+    for(let persona of personas){
+        console.log(persona);
+        texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
+    }
+    document.getElementById('personas').innerHTML = texto;
+}
