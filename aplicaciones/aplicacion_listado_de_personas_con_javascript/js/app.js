f571381bc436009cdbf98116d856093d744b1f63
joseLeider <180058244+joseLeider@users.noreply.github.com>
2025-03-22 23:21:46 -0500

    Agregar Formulario agregar nueva persona

--- aplicaciones/aplicacion_listado_de_personas_con_javascript/js/app.js
+++ aplicaciones/aplicacion_listado_de_personas_con_javascript/js/app.js
@@ -12,3 +12,22 @@ function mostrarPersonas(){
     }
     document.getElementById('personas').innerHTML = texto;
 }
+
+function agregarPersona(){
+    // Obtiene el formulario con el nombre 'forma'
+    const forma = document.forms['forma'];
+    // Obtiene los valores de los campos 'nombre' y 'apellido'
+    const nombre = forma['nombre'];
+    const apellido = forma['apellido'];
+    if(nombre.value != '' && apellido.value != ''){
+        // Crea un nuevo objeto Persona con los valores ingresados
+        const persona = new Persona(nombre.value, apellido.value);
+        console.log(persona);
+        // Agrega la nueva persona al array 'personas'
+        personas.push(persona);
+        // Actualiza la lista de personas en la interfaz
+        mostrarPersonas();
+    }else{
+        console.log('No hay información que agregar');
+    }
+}
